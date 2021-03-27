18d2d5463b95a361856d01f9a874258236a35568
TJ Hutchinson <tjhutch92@gmail.com>
2021-03-26 23:21:31 -0700

    Token testing working

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,35 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 contract Token {
-    string public name = "My Name";
+    using SafeMath for uint;
+
+    //Variables
+    string public name = "Teej Token";
+    string public symbol = "TEEJ";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    //Track balances
+    mapping(address => uint256) public balanceOf;
+    
+    // Events
+    event Transfer(address from, address to, uint256 value);
+
+    constructor() public {
+    totalSupply = 1000000 * (10 ** decimals);
+    balanceOf[msg.sender] = totalSupply;
 }
+
+    //Send tokens
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0));
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+}
+
