1673c0215baf800e3ed86e49bd17a0efb80bcc80
TJ Hutchinson <tjhutch92@gmail.com>
2021-03-30 19:16:02 -0700

    Exchange contract almost complete

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -7,6 +7,10 @@ contract Exchange {
     uint256 public feePercent; //the fee percentage
     address constant ETHER = address(0); //store Ether in tokens mapping with blank address
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order) public orders; // a way to store the order
+    uint256 public orderCount;
+    mapping(uint256 => bool) public orderCancelled;
+
     // events
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(
@@ -15,6 +19,37 @@ contract Exchange {
         uint256 amount,
         uint256 balance
     );
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+        );
+    event Cancel(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+        );
+    
+    //a way to model the order
+    struct _Order {
+        uint256 id;
+        address user;
+        address tokenGet;
+        uint256 amountGet;
+        address tokenGive;
+        uint256 amountGive;
+        uint256 timestamp;
+    }
+
+
     constructor(address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
@@ -58,6 +93,23 @@ contract Exchange {
     {
         return tokens[_token][_user];
     }
+
+    //add the order to storage
+    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+        orderCount = orderCount.add(1);
+        orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+        emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    }
+
+    function cancelOrder(uint256 _id) public{
+        _Order storage _order = orders[_id];
+        //Must be "my" order
+        require(address(_order.user) == msg.sender);
+        //Must be a valid order
+        require(_order.id == _id);
+        orderCancelled[_id] = true;
+        emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, now);
+    }
 }
 /*
 TODO:
@@ -67,8 +119,8 @@ TODO:
     [x] Deposit tokens
     [x] Withdrawl tokens
     [x] Check balances
-    [] Make Order
-    [] Cancel Order
+    [x] Make Order
+    [x] Cancel Order
     [] Fill Order
     [] Charge fees
 */
